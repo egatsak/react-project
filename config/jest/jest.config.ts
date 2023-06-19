39577516bf1e0059d04379fba1f2c88e1381cb7e
Eugene Gatsak <greenglaz@gmail.com>
2023-06-19 12:29:46 +0200

    feat: add jest reporter

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -60,7 +60,18 @@ export default {
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     transformIgnorePatterns: ["\\\\node_modules\\\\", "\\.pnp\\.[^\\\\]+$"],
-
+    reporters: [
+        "default",
+        [
+            "jest-html-reporters",
+            {
+                publicPath: "<rootDir>/reports/unit",
+                filename: "report.html",
+                openReport: true,
+                inlineSource: true,
+            },
+        ],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
