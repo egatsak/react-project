5e3a5fb4fefb2839de9d27b4fd1b8860b43d38e4
Eugene Gatsak <greenglaz@gmail.com>
2023-08-02 19:25:37 +0200

    feat: add circular dependency plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CircularDependencyPlugin from "circular-dependency-plugin";
 import CopyPlugin from "copy-webpack-plugin";
 import { BuildOptions } from "./types/config";
 
@@ -33,6 +34,10 @@ export function buildPlugins({
         }),
         new CopyPlugin({
             patterns: [{ from: paths.locales, to: paths.buildLocales }],
+        }),
+        new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            failOnError: true,
         }),
     ];
 
