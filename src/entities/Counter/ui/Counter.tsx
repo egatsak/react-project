0a69ffe9acbff74020cbc1b8742f76eac8b56864
Eugene Gatsak <greenglaz@gmail.com>
2023-08-16 13:45:12 +0200

    feat: add helpers for slice, selectors, actions

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,28 +1,33 @@
 import { FC } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Button } from "@/shared/ui/Button/Button";
-import { counterActions } from "../model/slice/counterSlice";
-import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
+import { useCounterActions } from "../model/slice/counterSlice";
+import { useCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
 
 export const Counter: FC = () => {
     const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
+    const counterValue = useCounterValue();
 
-    const increment = () => {
-        dispatch(counterActions.increment());
+    const { add, decrement, increment } = useCounterActions();
+    const handleIncrement = () => {
+        increment();
     };
 
-    const decrement = () => {
-        dispatch(counterActions.decrement());
+    const handleDecrement = () => {
+        decrement();
+    };
+
+    const handleAddFive = () => {
+        add(5);
     };
 
     return (
         <div>
             <h1 data-testid="value-title">{counterValue}</h1>
-            <Button onClick={increment} data-testid="increment-btn">
+            <Button onClick={handleIncrement} data-testid="increment-btn">
                 +
             </Button>
-            <Button onClick={decrement} data-testid="decrement-btn">
+            <Button onClick={handleDecrement} data-testid="decrement-btn">
                 -
             </Button>
         </div>
